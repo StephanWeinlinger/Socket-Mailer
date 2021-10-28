6496d3475c3b94d5ec7fc2a1fefbe1da1fb04fb5
StephanWeinlinger <stephan.weinlinger@gmail.com>
2021-10-28 16:03:05 +0200

    Add project files.

---
+++ twmailer-client/twmailer-client/twmailer-client.cpp
@@ -0,0 +1,12 @@
+﻿// twmailer-client.cpp : Defines the entry point for the application.
+//
+
+#include "twmailer-client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
